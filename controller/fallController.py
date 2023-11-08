0541332ab6ae582630240a2e70f242a272226f7a
John Hiesey <john@hiesey.com>
2023-11-07 20:41:29 -0800

    Add brightness for fallController

--- controller/fallController.py
+++ controller/fallController.py
@@ -7,24 +7,32 @@ from lightlib.abstractLightController import *
 class FallController(AbstractLightController):
     def __init__(self, port):
         super(FallController, self).__init__(port, 60, 30, 0, False)
+        self._bright = 0
     def colorListUpdate(self, currTime, colors):
+        if currTime % 1 == 0:
+            try:
+                with open('brightness.txt', 'r') as f:
+                    self._bright = min(max(float(f.readline()), 0), 1)
+            except Exception as e:
+                print("Could not read brightness configuration: %s" % (e,))
+
         for i, color in enumerate(colors):
             c = i % 3
             if c == 0:
                 color.r = 13
                 color.g = 0
                 color.b = 0
-                color.bright = 204
+                color.bright = 204 * self._bright
             elif c == 1:
                 color.r = 13
                 color.g = 7
                 color.b = 0
-                color.bright = 204
+                color.bright = 150 * self._bright
             elif c == 2:
                 color.r = 11
                 color.g = 1
                 color.b = 0
-                color.bright = 50
+                color.bright = 50 * self._bright
 
 controller = FallController('/dev/ttyACM0')
 controller.runUpdate()
