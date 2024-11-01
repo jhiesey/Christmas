75f5568ae104d021770f21c04cbfd4b4fe4f351f
John Hiesey <john@hiesey.com>
2024-10-31 18:42:52 -0700

    Tweak flickering

--- controller/orangeController.py
+++ controller/orangeController.py
@@ -6,11 +6,11 @@ from lightlib.abstractLightController import *
 
 class OrangeController(AbstractLightController):
     def __init__(self, port):
-        super(OrangeController, self).__init__(port, 60, 0.25, 0, False)
+        super(OrangeController, self).__init__(port, 60, 0.1, 0, False)
     def colorListUpdate(self, currTime, colors):
-        bright = 0xd
+        bright = 0xcc
         # Make lights flickery
-        if random.random() < 0.05:
+        if random.random() < 0.01:
             bright = 0
 
         for color in colors:
