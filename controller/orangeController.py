88844582cbec2348b3de1607a906d966208c6f94
John Hiesey <john@hiesey.com>
2024-10-31 18:43:40 -0700

    Individual light flickering

--- controller/orangeController.py
+++ controller/orangeController.py
@@ -8,12 +8,12 @@ class OrangeController(AbstractLightController):
     def __init__(self, port):
         super(OrangeController, self).__init__(port, 60, 0.1, 0, False)
     def colorListUpdate(self, currTime, colors):
-        bright = 0xcc
-        # Make lights flickery
-        if random.random() < 0.01:
-            bright = 0
-
         for color in colors:
+            bright = 0xcc
+            # Make lights flickery
+            if random.random() < 0.01:
+                bright = 0
+
             color.r = 0xd
             color.g = 0x1
             color.b = 0x0
