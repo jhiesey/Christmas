38282596d43e78f1b2a62daba4789f2c63457156
John Hiesey <john@hiesey.com>
2024-10-31 18:39:01 -0700

    Make halloween lights flickery

--- controller/orangeController.py
+++ controller/orangeController.py
@@ -6,12 +6,19 @@ from lightlib.abstractLightController import *
 
 class OrangeController(AbstractLightController):
     def __init__(self, port):
-        super(OrangeController, self).__init__(port, 60, 30, 0, False)
+        super(OrangeController, self).__init__(port, 60, 0.25, 0, False)
     def colorListUpdate(self, currTime, colors):
+        bright = 0xd
+        # Make lights flickery
+        if random.random() < 0.05:
+            bright = 0
+
         for color in colors:
             color.r = 0xd
             color.g = 0x1
             color.b = 0x0
+            color.bright = bright
+            color.forceBright = True
 
 controller = OrangeController('/dev/ttyACM0')
 controller.runUpdate()
